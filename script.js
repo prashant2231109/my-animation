2e96d6b85996b7557384a2563078b4047679ba50
prashant2231109 <prashant2231109@akgec.ac.in>
2023-10-02 15:49:20 +0530

    Basic Structures of JS

---
+++ script.js
@@ -0,0 +1,26 @@
+var arr=[
+{
+dp:"picp.png",story:"pic2.jpg"
+
+},
+{
+    dp:"pic3.png",story:"pic3.pg"
+    
+    },
+    {
+        dp:"srk.avif",story:"srk.jpg"
+        
+        },
+        {
+            dp:"pro.png",story:"pro1.JPG"
+            
+            },
+]
+arr.forEach(function(elem){
+
+    console.log(elem.dp)
+}
+    
+    
+    
+    )
